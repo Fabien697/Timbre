e5c0d5dd70a007a029a661311981f1a616e70daa
Fabien <bacfabien@yahoo.com>
2020-12-07 17:12:46 +0100

    réorganisation

---
+++ w3.js
@@ -0,0 +1,7 @@
+function amis () {
+    document.getElementById("reponse").innerHTML = "Bienvenue Mon Amis"
+}
+
+function ennemie () {
+    document.getElementById("reponse").innerHTML = "Dommage, une prochaine fois peut-être"
+}
